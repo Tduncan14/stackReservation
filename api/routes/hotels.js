7b4072c1e34802aaedbecd587ba0f6d4730f24e9
tduncan14 <nbatre1@yahoo.com>
2022-05-09 13:20:39 -0700

    keep moving forward

--- api/routes/hotels.js
+++ api/routes/hotels.js
@@ -52,7 +52,7 @@ router.put("/update/:id", async (req,res) => {
 
     try{
         // updating the req.body
-        const updatedHotel = await hotel.findByIdAndUpdate(req.params.id,{$set:req.body})
+        const updatedHotel = await hotel.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
         res.status(200).json(updatedHotel)
     }
     catch(err){
@@ -67,16 +67,74 @@ router.put("/update/:id", async (req,res) => {
 
 
 // delete
+router.delete("/:id", async (req,res) =>{
+
+    try{
+         await hotel.findByIdAndDelete(req.params.id)
+         res.status(200).json({
+             msg:'the information has been successfully deleted'
+         })
+
+    }
+
+    catch(err){
+
+        res.status(404).json({
+            msg:'what you are looking for doesnt exist'
+
+        })
+         
+    }
+})
+
+
+
 // get a single hotel
+
+
+
 // get all the hotels
 
 
 
-router.get('/', (req,res) =>{
+router.get('/:id',  async (req,res) =>{
 
+    try{
+        const singleHotel = await hotel.findById(req.params.id)
+        res.status(200).json({
+            msg:'hotel found',
+            singleHotel
+        })
+    }
+    catch(err){
 
+        res.status(404).json({
+            msg:"their isn't a hotel that matches this database"
+        })
+    }
 
-    res.send('this is the hotels apis');
+})
+
+//  find all the hotels
+
+router.get("/", async (req,res) =>{
+
+    try{
+       let hotels = await hotel.find()
+        res.status(200).json({
+            msg:'list of hotels',
+            hotels,
+
+        })
+
+    }
+
+    catch(err){
+        res.status(400).json({
+            msg:'their is a technical problem going on '
+        })
+
+    }
 })
 
 
