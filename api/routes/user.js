08c32f70192748da0e64fefa6a17fac13578aad2
tduncan14 <nbatre1@yahoo.com>
2022-05-11 15:26:32 -0700

    keep moving forward

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,8 +1,35 @@
 const express = require('express');
-const { getAllUser, updateUser, getUser, deleteUser } = require('../controllers/user');
 
+const { getAllUser, updateUser, getUser, deleteUser} = require('../controllers/user');
+const {verifyToken, verifyUser, verifyAdmin} = require('../utils/verifyToken');
 const router = express.Router()
 
+
+
+// router.get('/checkauthenication',verifyToken,(req,res,next) =>{
+
+
+//     res.send('hello user, you are logged in ')
+
+
+
+// }
+// )
+
+
+// router.get("/checkuser/:id",verifyAdmin, (req,res,next) => {
+
+//     res.send('you can logged in and delete your account')
+
+
+
+// })
+
+
+// router.get('/checkadmin/:id', verifyUser,(req,res,next) => {
+
+//     res.send('hello admin, you are logged in and you can delete any account')
+// })
 
 
 router.get('/',getAllUser) 
---
+++ api/utils/verifyToken.js
@@ -0,0 +1,70 @@
+const jwt = require('jsonwebtoken');
+const { createError } = require('./error');
+
+
+
+// verifying the token
+
+exports.verifyToken = (req,res,next) => {
+
+    const token  = req.cookies.access_token;
+
+    if(!token){
+        return next(createError(401,"you are not authenicated"))
+    }
+
+    jwt.verify(token,process.env.JWT_SECRET,(err,user) =>{
+        if(err){
+            return next(createError(403,'Token is not valid'))
+        }
+
+        req.user = user;
+        next()
+    })
+
+
+}
+
+
+exports.verifyUser = (req,res,next) => {
+
+    this.verifyToken(req,res,()=>{
+
+
+        // check if its the user or not
+        if(req.user.id === req.params.id || req.user.isAdmin){
+            next()
+        }
+
+        else{
+            return next(createError(403,'you are not authorized'))
+        }
+    })
+
+
+}
+
+
+//  see if the user isAdmin
+
+
+exports.verifyAdmin = (req,res,next) => {
+
+    this.verifyToken(req,res, () => {
+
+
+        if(req.user.isAdmin){
+            next()
+        }
+
+        else{
+            return next(createError(403,"you are not authorized"))
+        }
+    })
+
+
+
+
+
+
+}
